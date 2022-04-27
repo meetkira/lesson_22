cf6d071f47ab1208ead27aceb5b33abd85b3c3b4
Кристи <kristi.2001@yandex.ru>
2022-04-27 12:52:24 +0300

    bad_smell_call_chain accomplished

--- part1/practice/bad_smell_call_chain/main.py
+++ part1/practice/bad_smell_call_chain/main.py
@@ -2,44 +2,21 @@
 # оперировали внутренним состоянием, 
 # а не использовали цепочку вызовов объектов
 
-class Room:
-    def get_name(self):
-        return 42
-
-
-class Street:
-    def get_room(self) -> Room:
-        return Room()
-
-
-class City:
-    def get_street(self) -> Street:
-        return Street()
-
-    def population(self):
-        return 100500
-
-
-class Country:
-    def get_city(self) -> City:
-        return City()
-
-
-class Planet:
-    def get_contry(self) -> Country:
-        return Country()
-
 
 class Person:
-    def __init__(self):
-        self.planet = Planet()
+    def __init__(self, room_number: int, city_population: int):
+        self._room_number = room_number
+        self._city_population = city_population
 
     def get_person_room(self):
-        return self.planet.get_contry().get_city().get_street().get_room().get_name()
+        return self._room_number
 
     def get_city_population(self):
-        return self.planet.get_contry().get_city().population()
-
+        return self._city_population
 
 # после выполнения задания попробуйте
 # сделать экземпляр класса person и вызвать новые методы.
+if __name__ == "__main__":
+    person = Person(room_number=1, city_population=150000)
+    print(person.get_person_room())
+    print(person.get_city_population())
